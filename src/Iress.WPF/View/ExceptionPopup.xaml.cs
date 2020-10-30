3f917049f47a211350a39d420b55864fad9e2d20
AlexPi <alex.pigida@outlook.com>
2020-10-29 21:12:53 -0400

    Automatic handling of UI errors upon absence of the user.

--- src/Iress.WPF/View/ExceptionPopup.xaml.cs
+++ src/Iress.WPF/View/ExceptionPopup.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Iress.Sys.Ext;
 using System;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace Iress.WPF.View
@@ -19,13 +20,11 @@ namespace Iress.WPF.View
         innrMsgs.Text = $"{ex.InnerMessages()}";
       };
     }
-    void onClose(object s, RoutedEventArgs e) => Close(); // close popup and continue app execution
+    async void onLoaded(object s, RoutedEventArgs e) { await Task.Delay(180000); onCloseAndContinueExecution(s, e); }
+    void onCloseAndContinueExecution(object s, RoutedEventArgs e) => Close(); // close popup and continue app execution
     void onShutdown(object s, RoutedEventArgs e) => Application.Current.Shutdown();
     void T4_MouseDown(object s, System.Windows.Input.MouseButtonEventArgs e) => onCopyClose(s, e);
-    void onCopyClose(object s, RoutedEventArgs e)
-    {
-      Clipboard.SetText($"{callerFL.Text}\r\n{methodNm.Text}\r\n{optnlMsg.Text}\r\n{innrMsgs.Text}");
-      onClose(s, e);
-    }
+    void onCopyClose(object s, RoutedEventArgs e) { Clipboard.SetText($"{callerFL.Text}\r\n{methodNm.Text}\r\n{optnlMsg.Text}\r\n{innrMsgs.Text}"); onCloseAndContinueExecution(s, e); }
+
   }
 }
